8c8b62a602a2ed1cb7d25cf0443965c2476172c6
Juan Latorre <juanlatorre@unicauca.edu.co>
2025-12-09 22:50:46 -0500

    Src + Docker

---
+++ src/main/java/com/unicauca/trabajo/grado/microservices/TrabajoGradoMicroservices.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.unicauca.trabajo.grado.microservices;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class TrabajoGradoMicroservices {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
